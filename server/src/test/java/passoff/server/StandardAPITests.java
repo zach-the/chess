493798664561cb45e61eab616c38d24e71f052cd
zb256 <zachzbennett@gmail.com>
2024-10-30 12:02:07 -0600

    setup complete... i hope

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Locale;
-
+a
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StandardAPITests {
 
