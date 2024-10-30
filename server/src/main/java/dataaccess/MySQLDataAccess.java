f8783bc4ff5dcf19bb06ae7a2a221ccc0a28881e
zb256 <zachzbennett@gmail.com>
2024-10-30 13:43:27 -0600

    the beginnings of database

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -8,6 +8,8 @@ import model.UserData;
 
 import java.sql.SQLException;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 
 public class MySQLDataAccess implements DataAccess {
 
@@ -16,7 +18,10 @@ public class MySQLDataAccess implements DataAccess {
     }
 
     // CLASS OVERRIDES START HERE
-    public void addUser(UserData user) {}
+    public void addUser(UserData user) {
+        var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        var id = executeUpdate(statement, user.username(), user.password(), user.email());
+    }
     public void addAuth(AuthData auth) {}
     public void deleteAuth(String authToken) {}
     public UserData getUser(String username) { return null; }
@@ -30,18 +35,65 @@ public class MySQLDataAccess implements DataAccess {
     public void deleteGameData() {}
     // CLASS OVERRIDES END HERE
 
+    private int executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS  pet (
-              `id` int NOT NULL AUTO_INCREMENT,
-              `name` varchar(256) NOT NULL,
-              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-              `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`id`),
-              INDEX(type),
-              INDEX(name)
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(password),
+              INDEX(email)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
+//            ,
+//            """
+//            CREATE TABLE IF NOT EXISTS  auths (
+//              `auth` varchar(256) NOT NULL,
+//              `username` varchar(256) NOT NULL,
+//              `json` TEXT DEFAULT NULL,
+//              PRIMARY KEY (`auth`),
+//              INDEX(username)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+//            """
+//            ,
+//            """
+//            CREATE TABLE IF NOT EXISTS  games (
+//            `id` int NOT NULL AUTO_INCREMENT,
+//            `whiteUsername` varchar(256) NOT NULL,
+//            `blackUsername` varchar(256) NOT NULL,
+//            `gameName` varchar(256) NOT NULL,
+//            `json` TEXT DEFAULT NULL,
+//            PRIMARY KEY (`auth`),
+//            INDEX(username)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+//            """
     };
 
     private void configureDatabase() throws DataAccessException, ResponseException {
