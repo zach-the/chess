38b31d5780eec44690eb4d36dfacd0609e25becd
zb256 <zachzbennett@gmail.com>
2024-11-02 14:14:40 -0600

    dataaccess

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,18 +1,19 @@
 package dataaccess;
+import exception.ResponseException;
 import model.*;
 
 public interface DataAccess {
-    void addUser(UserData user);
-    void addAuth(AuthData auth);
-    void deleteAuth(String authToken);
-    UserData getUser(String username);
-    AuthData getAuth(String authToken);
-    void createGame(Integer gameID, GameData gameData);
-    GameData getGame(Integer gameID);
-    void updateGame(Integer gameID, GameData gameData);
-    GameList listGames();
+    void addUser(UserData user) throws ResponseException, DataAccessException;
+    void addAuth(AuthData auth) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void createGame(Integer gameID, GameData gameData) throws DataAccessException;
+    GameData getGame(Integer gameID) throws DataAccessException;
+    void updateGame(Integer gameID, GameData gameData) throws DataAccessException;
+    GameList listGames() throws DataAccessException;
 
-    void deleteUserData();
-    void deleteAuthData();
-    void deleteGameData();
+    void deleteUserData() throws DataAccessException;
+    void deleteAuthData() throws DataAccessException;
+    void deleteGameData() throws DataAccessException;
 }
