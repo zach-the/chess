903bfb90107f326d95ff9a8fd487c29976462793
zb256 <zachzbennett@gmail.com>
2024-11-14 23:58:02 -0700

    escapesequences alterations

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -5,8 +5,17 @@ package ui;
  */
 public class EscapeSequences {
 
-    private static final String UNICODE_ESCAPE = "\u001b";
+    private static final String UNICODE_ESCAPE = "\u001B";
     private static final String ANSI_ESCAPE = "\033";
+
+    public static final String GREEN = UNICODE_ESCAPE + "[32m";
+    public static final String BLUE = UNICODE_ESCAPE + "[34m";
+    public static final String RED = UNICODE_ESCAPE + "[31m";
+    public static final String PURPLE = UNICODE_ESCAPE + "[035m";
+    public static final String WHITE = UNICODE_ESCAPE + "[037m";
+
+    public static final String RESET = UNICODE_ESCAPE + "[0m";
+
 
     public static final String ERASE_SCREEN = UNICODE_ESCAPE + "[H" + UNICODE_ESCAPE + "[2J";
     public static final String ERASE_LINE = UNICODE_ESCAPE + "[2K";
