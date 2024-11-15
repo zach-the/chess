ef0b1c5ad40c4095287139040b66a9e8c4920007
zb256 <zachzbennett@gmail.com>
2024-11-14 23:57:25 -0700

    main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import ui.EscapeSequences;
+import ui.PreloginUI;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        var serverURL = "http://localhost:8080";
+        if (args.length ==1) {
+            serverURL = args[0];
+        }
+        System.out.println(EscapeSequences.PURPLE + "♕ Welcome to CS 240 Chess! ♕" + EscapeSequences.BLUE);
+        new PreloginUI(serverURL).repl();
     }
 }
