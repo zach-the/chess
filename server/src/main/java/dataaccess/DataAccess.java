e474a8563e9b5a6ad464e5bbc8cf0b2b9c9119b1
zb256 <zachzbennett@gmail.com>
2024-11-14 23:58:19 -0700

    dataaccess bug fixes

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -2,12 +2,14 @@ package dataaccess;
 import exception.ResponseException;
 import model.*;
 
+import java.sql.SQLException;
+
 public interface DataAccess {
     void addUser(UserData user) throws ResponseException, DataAccessException;
     void addAuth(AuthData auth) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
     UserData getUser(String username) throws DataAccessException;
-    AuthData getAuth(String authToken) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException, SQLException;
     void createGame(Integer gameID, GameData gameData) throws DataAccessException;
     GameData getGame(Integer gameID) throws DataAccessException;
     void updateGame(Integer gameID, GameData gameData) throws DataAccessException;
